cc5f5b6f362502c6683a200da32dcdef5d9ec0d3
BartoszChronowski <b.chronowski@sunscrapers.com>
2023-03-20 00:33:26 +0100

    chore(tests): Translate tests to typescript. File cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
-<script setup>
-import { RULE } from "@/domain/password/rules";
-
-const rules = Object.values(RULE);
+<script setup lang="ts">
 </script>
 
 <template>
-  <div>
+  <div class="main">
     <input data-test="password-field" />
-
-    <ul>
-      <li
-        v-for="rule in rules"
-        :key="rule"
-        :data-test-rule-indicator="rule"
-        class="password-hint__rule password-hint__rule--fail"
-      >
-        HINT
-      </li>
-    </ul>
-
     <span data-test="validation-summary">Strong or Weak?</span>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.main {
+  background-color: var(--color-base-white);
+}
+</style>
